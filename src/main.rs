3a3b528f855723887e30a68446e006b861044401
kevetka <redpatrik2007@gmail.com>
2025-11-06 22:52:00 +0300

    Adding a solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
 fn main() {
+    let mut input = String::new();
 
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let path = input.trim();
+            match std::fs::read(path) {
+                Ok(_) => println!("success"),
+                Err(_) => println!("failure"),
+            }
+        }
+        Err(_) => println!("failure"),
+    }
 }
